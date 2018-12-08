4bbf90915e2f5061593582272ebb189d4456634a
Ali Faris <alihcompiler@gmail.com>
2018-12-08 21:25:57 +0300

    removing some unnecessarily file , add json lib

--- app/src/main/java/com/baghdadit/simplehttp/ISimpleHttp.java
+++
@@ -1,9 +0,0 @@
-package com.baghdadit.simplehttp;
-
-public interface ISimpleHttp {
-
-    public SimpleHttpResponse post();
-
-    public SimpleHttpResponse get();
-
-}
--- app/src/main/java/com/baghdadit/simplehttp/SimpleHttpResponseException.java
+++
@@ -1,7 +0,0 @@
-package com.baghdadit.simplehttp;
-
-import java.io.IOException;
-
-public class SimpleHttpResponseException extends IOException {
-
-}
