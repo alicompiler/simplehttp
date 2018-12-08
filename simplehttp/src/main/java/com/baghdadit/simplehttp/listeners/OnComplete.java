f0f8ccbac229940a26e9758a289a812b4dac4098
Ali Faris <alihcompiler@gmail.com>
2018-12-08 21:22:44 +0300

    adding OnComplete , OnError interfaces

---
+++ simplehttp/src/main/java/com/baghdadit/simplehttp/listeners/OnComplete.java
@@ -0,0 +1,7 @@
+package com.baghdadit.simplehttp.listeners;
+
+import com.baghdadit.simplehttp.SimpleHttpResponse;
+
+public interface OnComplete {
+    public void onComplete(SimpleHttpResponse response);
+}
---
+++ simplehttp/src/main/java/com/baghdadit/simplehttp/listeners/OnError.java
@@ -0,0 +1,5 @@
+package com.baghdadit.simplehttp.listeners;
+
+public interface OnError {
+    public void onError(Exception e);
+}
